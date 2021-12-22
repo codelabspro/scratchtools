c7cb0530f86a5481c417d192bb5775329b7bff9e
Arunabh Das <arunabhdas@gmail.com>
2021-12-22 16:09:32 -0500

    Improved scratchtools

--- scratchtools.rb
+++ scratchtools.rb
@@ -1,10 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-file = File.open("input.xml")
-xmldoc = Nokogiri::XML(file)
-file.close
-print xmldoc 
+inputfile = File.open("input.xml")
+inputfilexmldoc = Nokogiri::XML(inputfile)
+inputfile.close
+print inputfilexmldoc 
 
 xml = Nokogiri::XML::Builder.new { |xml| 
 
@@ -18,3 +18,24 @@ xml = Nokogiri::XML::Builder.new { |xml|
 }.to_xml
 
 File.open("output.md", "w") { |f| f.write(xml)}
+
+
+inputconstructdoc = File.open("inputfile.xml")
+constructdoc = Nokogiri::HTML <<-EOHTML
+<?xml version="1.0" encoding="utf-8"?>
+ <body>
+    <item>
+      <time>05.04.2011 9:53:23</time>
+      <infrastructure>cloud</infrastructure>
+      <database>postgres</database>
+      <frontend>Next</frontend>
+      <backend>Python</backend>
+    </item>
+  </body>
+</xml>
+EOHTML
+
+hammer = constructdoc.at_css "time"
+outputfile = File.new("outputfile.xml", "w")
+outputfile.puts constructdoc.to_html
+outputfile.close
